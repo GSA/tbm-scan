5c84618dc9ebcda124dbab9804713a6b825597a0
Scott McAllister <scott.mcallister@gsa.gov>
2019-07-25 13:39:03 -0400

    TST: test col filter logic :white_check_mark: (#2)

--- tests/writer_test.py
+++ tests/writer_test.py
@@ -17,12 +17,12 @@ class WriterTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_transform_data(self):
+    def test_transform_data_field_filter(self):
         data = ('20190501',
                 {'PRESOL': [{'AGENCY': 1, 'b': 2}, {'AGENCY': 3, 'b': 4}, {'AGENCY': 1, 'b': 2, 'c': 3}],
                 'MOD': [{'AGENCY': 1, 'b': 2}, {'AGENCY': 3, 'b': 4}, {'AGENCY': 1, 'b': 2, 'c': 3, 'd': 1}]}
         )
-        result = transform_data(data)
+        result = transform_data(data, field_filter = True)
         expected = [ {'notice type': 'PRESOL', 'fbo date': '20190501', 'AGENCY': 1},
                      {'notice type': 'PRESOL', 'fbo date': '20190501', 'AGENCY': 3},
                      {'notice type': 'PRESOL', 'fbo date': '20190501', 'AGENCY': 1},
@@ -30,3 +30,20 @@ class WriterTestCase(unittest.TestCase):
                      {'notice type': 'MOD', 'fbo date': '20190501', 'AGENCY': 3},
                      {'notice type': 'MOD', 'fbo date': '20190501', 'AGENCY': 1}]
         self.assertEqual(result, expected)
+
+    def test_transform_data(self):
+        data = ('20190501',
+                {'PRESOL': [{'AGENCY': 1, 'b': 2}, {'AGENCY': 3, 'b': 4}, {'AGENCY': 1, 'b': 2}],
+                'MOD': [{'AGENCY': 1, 'b': 2}, {'AGENCY': 3, 'b': 4}, {'AGENCY': 1, 'b': 2}]}
+        )
+        result = transform_data(data)
+        expected = [ {'notice type': 'PRESOL', 'fbo date': '20190501', 'AGENCY': 1, 'b':2},
+                     {'notice type': 'PRESOL', 'fbo date': '20190501', 'AGENCY': 3, 'b':4},
+                     {'notice type': 'PRESOL', 'fbo date': '20190501', 'AGENCY': 1, 'b':2},
+                     {'notice type': 'MOD', 'fbo date': '20190501', 'AGENCY': 1, 'b':2},
+                     {'notice type': 'MOD', 'fbo date': '20190501', 'AGENCY': 3, 'b':4},
+                     {'notice type': 'MOD', 'fbo date': '20190501', 'AGENCY': 1, 'b':2}]
+        self.assertEqual(result, expected)
+
+if __name__ == '__main__':
+    unittest.main()
